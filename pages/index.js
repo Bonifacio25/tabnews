ee7700be02efc50a6ff4b0d1b72d669859e50966
Felipe Bonifácio <bonifelipe7@gmail.com>
2026-03-27 10:26:24 -0300

    Adicionando scripts de lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home () {
-    return <h1>Olá mundo, agora está online!</h1>
+function Home() {
+  return <h1>Olá mundo, agora está online!</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
