ca12b43f617b338d321fb8e40ee2f144ed515e27
Felipe Bonifácio <bonifelipe7@gmail.com>
2026-03-29 15:18:15 -0300

    Página em contrução

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,30 @@
 function Home() {
-  return <h1>Olá mundo, agora está online!</h1>;
+  return (
+    <div
+      style={{
+        margin: 0,
+        height: "90vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        //background: "linear-gradient(135deg, #0e0c3d, #9333ea)",
+        color: "#122142",
+        fontFamily: "Arial",
+      }}
+    >
+      <div
+        style={{
+          textAlign: "center",
+          background: "rgba(48, 43, 43, 0.1)",
+          padding: "80px",
+          borderRadius: "16px",
+        }}
+      >
+        <h1>🚧 Em Construção</h1>
+        <p>Estamos trabalhando para trazer algo incrível para você.</p>
+      </div>
+    </div>
+  );
 }
 
 function teste() {
