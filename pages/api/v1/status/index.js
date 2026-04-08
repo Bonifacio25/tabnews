e455b7b655445a183ac16caf3b6529bfd1ae0b4c
Felipe Bonifácio <bonifelipe7@gmail.com>
2026-04-08 18:29:50 -0300

    Dia 19

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,12 @@
-function status (request, response) {
-  response.status(200).json({chave: "São palavras do utf-8 !@#$%¨&* <?php ?> <br> <h1>TESTE</h1> <br>"});
+import database from "../../../../infra/database.js";
+
+async function status (request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+
+  console.log("DB:", process.env.POSTGRES_DB);
+  console.log("DATABASE:", process.env.POSTGRES_DATABASE);
+  
+  response.status(200).json({chave: result.rows});
 }
 
 export default status;
