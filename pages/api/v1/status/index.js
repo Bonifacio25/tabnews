1b683a8dade23cefe8d023c37a8294047cbf8de3
Felipe Bonifácio <bonifelipe7@gmail.com>
2026-04-14 19:26:19 -0300

    Adicionando o arquivo jsconfig

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,9 @@
-import database from "../../../../infra/database.js";
+import database from "infra/database.js"; 
 
 async function status (request, response) {
   const result = await database.query("SELECT 1 + 1 as sum;");
 
-  console.log("DB:", process.env.POSTGRES_DB);
-  console.log("DATABASE:", process.env.POSTGRES_DATABASE);
+  console.log(result.rows);
   
   response.status(200).json({chave: result.rows});
 }
